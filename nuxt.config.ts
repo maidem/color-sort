2863624217fda53dfe4d74a4313ddac411bbc8c1
maidem <connect@maidem.de>
2026-04-30 23:44:22 +0200

    fix: workaround for Nuxt 4.4.4 SPA dev server vite-node IPC bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-04-01',
   devtools: { enabled: true },
   ssr: false,
+  // Workaround für Nuxt 4.4.4 SPA dev-server bug
+  // (https://github.com/nuxt/nuxt/issues/34957)
+  experimental: {
+    viteEnvironmentApi: true
+  },
   modules: ['@pinia/nuxt', '@vueuse/nuxt', '@nuxtjs/tailwindcss'],
   css: ['~/assets/css/main.css'],
   app: {
